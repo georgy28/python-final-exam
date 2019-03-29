959dd79e9d6fcf9de380fd37e80fc0b5607c6c34
georgy28 <georgy_papadopoulos@yahoo.co.uk>
2019-03-29 03:28:03 +0200

    Added comment

--- merge_names.py
+++ merge_names.py
@@ -2,6 +2,7 @@ def unique_names(names1, names2):
     return list(set(names1).union(set(names2)))
 
 
+# test case
 names1 = ["Ava", "Emma", "Olivia"]
 names2 = ["Olivia", "Sophia", "Emma"]
 print(unique_names(names1, names2))
